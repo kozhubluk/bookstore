090adbcf73dc2531378626eef6a07944f4e4259d
Anastasia <nastya.awdeeva@yandex.ru>
2024-10-13 11:59:01 +0300

    Initial commit

---
+++ src/app/path/page.tsx
@@ -0,0 +1,9 @@
+import styles from '@/app/page.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <div>text {'dfsdf'} block</div>
+    </div>
+  );
+}
